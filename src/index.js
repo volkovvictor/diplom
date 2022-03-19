7a68defe997671fbd7e2c85a1e6d96a4843eeee8
Victor Volkov <vityan2504@gmail.com>
2022-03-19 17:20:03 +0300

    написал универсальную функцию для всех модалок

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@
 
 import popup from './modules/popup';
 
-popup();
+popup('#header .btn', '.header-modal', '.header-modal__close');
+popup('.service-button .btn', '.services-modal', '.services-modal__close');
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,5 +1,42 @@
-const popup = () => {
-   console.log("popup");
+import { animate } from './_helpers';
+
+const popup = (btn, popup, close) => {
+   const body = document.querySelector('body');
+   const headerModal = document.querySelector(popup);
+   const overlay = document.querySelector('.overlay');
+
+   overlay.style.opacity = 0;
+   headerModal.style.transform = 'translate(-50%, -50%) scale(0)';
+
+   const openPopup = () => {
+      overlay.style.display = "block";
+      headerModal.style.display = "block";
+      animate({
+         duration: 500,
+         timing(timeFraction) {
+            return timeFraction;
+         },
+         draw(progress) {
+            overlay.style.opacity = progress;
+            headerModal.style.transform = `translate(-50%, -50%) scale(${progress})`;
+         }
+      });
+   }
+
+   const closePopup = () => {
+      overlay.style.display = "none";
+      headerModal.style.display = "none";
+   }
+
+   body.addEventListener('click', (e) => {
+      if(e.target.matches(btn)) {
+         openPopup();
+      }
+      if(e.target.matches(close) ||
+         e.target.classList.contains("overlay")) {
+         closePopup();
+      }
+   });
 };
 
 export default popup;
