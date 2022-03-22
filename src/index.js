4912224dae521b87bbb92a53bc81c9684af48ee2
Victor Volkov <vityan2504@gmail.com>
2022-03-22 19:09:13 +0300

    написал функцию для слайдеров

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import popup from './modules/popup';
+import slider from './modules/slider';
 
 popup('#header .btn', '.header-modal', '.header-modal__close');
 popup('.service-button .btn', '.services-modal', '.services-modal__close');
+slider('benefits', '.benefits__item', '.benefits__arrow--right img', '.benefits__arrow--left img', 3);
+slider('services', '.service-block', '.services__arrow--right img', '.services__arrow--left img',  2);
