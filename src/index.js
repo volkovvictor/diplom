dca1bc6bffe0116cfd9081d6359fdf82140eecad
Victor Volkov <vityan2504@gmail.com>
2022-03-23 18:49:17 +0300

    написал функцию для зума

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
 import popup from './modules/popup';
 import slider from './modules/slider';
 import timer from './modules/timer';
+import zoom from './modules/zoom';
 
 popup('#header .btn', '.header-modal', '.header-modal__close');
 popup('.service-button .btn', '.services-modal', '.services-modal__close');
 slider('benefits', '.benefits__item', '.benefits__arrow--right img', '.benefits__arrow--left img', 3);
 slider('services', '.service-block', '.services__arrow--right img', '.services__arrow--left img',  2);
 timer('30 March 2022');
+zoom();
