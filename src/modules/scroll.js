0482ae1455bf001ff417791447e12ead0fa1e549
Victor Volkov <vityan2504@gmail.com>
2022-03-25 04:04:07 +0300

    доделал скролл

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,7 +1,32 @@
-const scroll = () => {
-   const scrollTop = document.querySelector('.smooth-scroll');
+import { animate } from './_helpers';
 
-   scrollTop.addEventListener('click', () => {
+const scroll = () => {
+   const offer = document.getElementById('offer');
+   const scrollTopBtn = document.querySelector('.smooth-scroll');
+
+   scrollTopBtn.style.display = 'none';
+   scrollTopBtn.style.opacity = '0';
+
+   window.addEventListener('scroll', () => {
+      const top = offer.offsetTop + offer.offsetHeight;
+
+      scrollTopBtn.style.display = 'none';
+
+      if (window.scrollY >= top) {
+         scrollTopBtn.style.display = 'block';
+         animate({
+            duration: 200,
+            timing(timeFraction) {
+               return timeFraction;
+            },
+            draw(progress) {
+               scrollTopBtn.style.opacity = progress;
+            }
+         });
+      }
+   });
+
+   scrollTopBtn.addEventListener('click', () => {
       document.body.scrollIntoView({
          behavior: 'smooth',
          block: 'start'
